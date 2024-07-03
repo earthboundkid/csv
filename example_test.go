4d2bd1eee90215c40caac88dfaa40a9d3ffb50e6
Carlana Johnson <mail@carlana.net>
2024-07-02 20:10:16 -0400

    Use iter.Seq2; update to v2; fix URL

--- example_test.go
+++ example_test.go
@@ -5,23 +5,24 @@ import (
 	"log"
 	"strings"
 
-	"github.com/carlmjohnson/csv"
+	"github.com/earthboundkid/csv/v2"
 )
 
-func ExampleFieldReader() {
+func Example() {
 	in := `first_name,last_name,username
 "Rob","Pike",rob
 Ken,Thompson,ken
 "Robert","Griesemer","gri"
 `
-	r := csv.NewFieldReader(strings.NewReader(in))
-
-	for r.Scan() {
-		fmt.Println(r.Field("username"))
+	csvopt := csv.Options{
+		Reader: strings.NewReader(in),
 	}
 
-	if err := r.Err(); err != nil {
-		log.Fatal(err)
+	for row, err := range csvopt.Rows() {
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(row.Field("username"))
 	}
 
 	// Output:
@@ -32,27 +33,25 @@ Ken,Thompson,ken
 
 // This example shows how csv.FieldReader can be configured to handle other
 // types of CSV files.
-func ExampleFieldReader_options() {
-	in := `first_name;last_name;username
-"Rob";"Pike";rob
+func ExampleOptions() {
+	in := `"Rob";"Pike";rob
 # lines beginning with a # character are ignored
 Ken;Thompson;ken
 "Robert";"Griesemer";"gri"
 `
-	r := csv.NewFieldReader(strings.NewReader(in))
-	r.Comma = ';'
-	r.Comment = '#'
-
-	for r.Scan() {
-		fmt.Println(r.Field("username"))
+	csvopt := csv.Options{
+		Reader:     strings.NewReader(in),
+		Comma:      ';',
+		Comment:    '#',
+		FieldNames: []string{"first_name", "last_name", "username"},
 	}
 
-	if err := r.Err(); err != nil {
+	rows, err := csvopt.ReadAll()
+	if err != nil {
 		log.Fatal(err)
 	}
+	fmt.Println(rows)
 
 	// Output:
-	// rob
-	// ken
-	// gri
+	// [map[first_name:Rob last_name:Pike username:rob] map[first_name:Ken last_name:Thompson username:ken] map[first_name:Robert last_name:Griesemer username:gri]]
 }
